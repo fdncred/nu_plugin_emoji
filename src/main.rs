7fb2d9c5fb4be57191fdf70fc09c38389ffea18c
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2024-01-15 07:45:20 -0600

    update to latest plugin api

--- src/main.rs
+++ src/main.rs
@@ -41,6 +41,7 @@ impl Plugin for Implementation {
     fn run(
         &mut self,
         name: &str,
+        _config: &Option<Value>,
         call: &EvaluatedCall,
         _input: &Value,
     ) -> Result<Value, LabeledError> {
